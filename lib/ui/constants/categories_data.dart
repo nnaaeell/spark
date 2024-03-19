936e3cfb9309703499ae78de5b47959a9e43b744
naelg <naellghanem9@gmail.com>
2024-03-19 15:10:48 +0300

    FIRST

---
+++ lib/ui/constants/categories_data.dart
@@ -0,0 +1,28 @@
+import '../../data/models/categories_model.dart';
+
+final List<CategoriesModel> categories = [
+  CategoriesModel(
+      title: "Ux Designer",
+      imagePath: "assets/ux_designer.png"),
+  CategoriesModel(
+      title: "Web Developer",
+      imagePath: "assets/web_developer.png"),
+  CategoriesModel(
+      title: "Marketing",
+      imagePath: "assets/marketing.png"),
+  CategoriesModel(
+      title: "App Developer",
+      imagePath: "assets/app_developer.png"),
+  CategoriesModel(
+      title: "Software Engineer",
+      imagePath: "assets/software_engineer.png"),
+  CategoriesModel(
+      title: "Product Manager",
+      imagePath: "assets/product_manager.png"),
+  CategoriesModel(
+      title: "Accountant",
+      imagePath: "assets/accountant.png"),
+  CategoriesModel(
+      title: "Graphic Designer",
+      imagePath: "assets/graphic_designer.png"),
+];
